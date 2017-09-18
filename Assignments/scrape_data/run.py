9ab9da1d32d695b005868acad84f61e9b934cf7f
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2017-09-18 12:45:41 -0500

    Update run.py

--- Assignments/scrape_data/run.py
+++ Assignments/scrape_data/run.py
@@ -1,10 +1,38 @@
-# -*- coding: utf-8 -*-
-import sys
-import requests
+#             Ajay Dinakar Kandavalli
+#             CMPS-5443 Data mining
+#                 Assignment-1   
+#         Scraping Data from a website     
+#              Language :python3,
+#   website choosed is www.allaboutcircuits.com(embedded projects website)  
+#*******************************************************************************
+#importing libraries
 import bs4
-from bs4 import BeautifulSoup
-import time
-import random
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as soup
 
-
-emp_details = []
+f=open("projects.csv","w")
+f.write("project Name, Published Time, Year, Author \n")
+#interating multiple pages and
+#getting the url stored in and
+for WPagenumb in range( 0, (5+1)*20, 20)[0:]:
+    WPagenumb=str(numb)
+    #first webpage
+    if WPagenumb is 0:
+        url="https://www.allaboutcircuits.com/projects/category/embedded/"
+    #pages other than first webpage    
+    else:    
+        url="https://www.allaboutcircuits.com/projects/category/embedded/"+ "P" + WPagenumb + "/"
+#opening connection and accessing the web page
+    uclient=ureq(url)
+    page_html=uclient.read()
+    uclient.close()
+#parse the html
+    page=soup(page_html,"html.parser")
+    projects=page.findAll("div",{"class":"row archive-container"})
+    
+    for project in projects:
+        projectTitle=(project.findNext("div",{"class":"col-lg-12 article-heading"}).h3.text)
+        projectTime=(project.findNext("span",{"class":"meta-timespan"}).text)
+        projectAuthor=(project.findNext("a",{"class":"article-author"}).text)
+        f.write(projectTitle +", " + projectTime+", "+ projectAuthor + "\n")
+f.close()
